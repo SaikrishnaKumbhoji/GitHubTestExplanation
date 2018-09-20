0e6c20072e336bd3dc8db40ce7635b2a3c732f3b
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-20 18:47:14 +0530

    MapFeature page Added

---
+++ GitHubSampleTest/MapFeature.swift
@@ -0,0 +1,13 @@
+//
+//  MapFeature.swift
+//  GitHubSampleTest
+//
+//  Created by Sai Krishna Kumbhoji on 9/20/18.
+//  Copyright © 2018 Sai Krishna Kumbhoji. All rights reserved.
+//
+
+import UIKit
+
+class MapFeature: NSObject {
+
+}
--- GitHubSampleTest/ViewController.swift
+++ GitHubSampleTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Added a line in ViewController")
     }
    
     @IBAction func buttonClicked(_ sender: Any) {
@@ -20,9 +21,12 @@ class ViewController: UIViewController {
         print("Saikrishna has Changed")
     }
     
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
     
