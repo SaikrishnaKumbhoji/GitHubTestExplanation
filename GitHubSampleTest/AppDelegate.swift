b0891a93f6c3f883658968bb1ceb9c0e283ab803
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-20 17:45:07 +0530

    Added a line in AppDelegate

--- GitHubSampleTest/AppDelegate.swift
+++ GitHubSampleTest/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("This is Sample Github testing")
         return true
     }
 
